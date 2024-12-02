8948f73f9ac84d1c108121ebc123900e6e91cda1
PiotrDudziewicz1TB <106385501+PiotrDudziewicz1TB@users.noreply.github.com>
2024-12-02 13:37:07 +0100

    Delete Panele directory

--- Panele/script.js
+++
@@ -1,9 +0,0 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    if (section) {
-        section.scrollIntoView({
-            behavior: 'smooth', // płynne przewijanie
-            block: 'start'      // wyrównanie do góry strony
-        });
-    }
-}
